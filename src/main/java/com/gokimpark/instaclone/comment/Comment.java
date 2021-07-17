5188909ee6ff302ad8ac7bf7b8f54fd46e865c05
evelyn82ny <evelyn82ny@gmail.com>
2021-07-18 06:46:36 +0900

    set @ManyToOne(post)

--- src/main/java/com/gokimpark/instaclone/comment/Comment.java
+++ src/main/java/com/gokimpark/instaclone/comment/Comment.java
@@ -1,21 +1,29 @@
 package com.gokimpark.instaclone.comment;
 
+import com.gokimpark.instaclone.post.Post;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.LastModifiedDate;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Comment {
     @Id @GeneratedValue
     @Column(name = "comment_id")
     private Long id;
 
-    @Column(name = "comment_writer_id")
-    private String UserId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
 
+    private String userId;
+
+    @Lob
     private String comment;
 
+    @LastModifiedDate
     private LocalDateTime CreatedTime;
-
-    public Comment(){}
-    public Comment(String UserId, Comment comment){}
 }
