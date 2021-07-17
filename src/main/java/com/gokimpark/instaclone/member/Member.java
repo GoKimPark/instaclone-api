f55b83210f31d7c1af25fff53289e398896c6e1e
evelyn82ny <evelyn82ny@gmail.com>
2021-07-18 06:45:05 +0900

    set unique

--- src/main/java/com/gokimpark/instaclone/member/Member.java
+++ src/main/java/com/gokimpark/instaclone/member/Member.java
@@ -1,45 +1,54 @@
 package com.gokimpark.instaclone.member;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.gokimpark.instaclone.account.MemberJoinInfo;
+import lombok.Getter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 
 @Entity
+@Getter
 public class Member {
     @Id
-    @Column(name = "member_email", nullable = false)
+    @Column(unique = true)
     private String email;
 
-    private String ProfilePhoto;
+    private String profileImageUrl;
 
-    @Column(nullable = false)
     private String name;
 
-    @Column(nullable = false)
+    @Column(unique = true)
     private String id;
 
-    private String Website;
+    private String website;
+
+    @Lob
     private String bio;
 
-    @Column(nullable = false)
-    private String PhoneNumber;
+    private Long postCnt;
+    private Long followerCnt;
+    private Long followingCnt;
+
+    private String phoneNumber;
     private String gender;
 
     @JsonIgnore
-    @Column(nullable = false)
     private String password;
 
-    public Member(){}
-    public Member(MemberJoinInfo member){
-        this.email = member.getEmail();
-        this.PhoneNumber = member.getPhoneNumber();
-        this.id = member.getUserId();
-        this.name = member.getUserName();
-        this.password = member.getPassword();
+    protected Member(){
+        this.postCnt = 0L;
+        this.followerCnt = 0L;
+        this.followingCnt = 0L;
     }
 
+    // set
+    public void postCountInc(){
+        this.postCnt++;
+    }
 
+    public void postCountDec(){
+        this.postCnt--;
+    }
 }
