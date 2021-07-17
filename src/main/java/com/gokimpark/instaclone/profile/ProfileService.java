388fa71cee8669d8dd29b5a4e2425c5edca47105
evelyn82ny <evelyn82ny@gmail.com>
2021-07-18 06:42:12 +0900

    created mapper(profile dto)

--- src/main/java/com/gokimpark/instaclone/profile/ProfileService.java
+++ src/main/java/com/gokimpark/instaclone/profile/ProfileService.java
@@ -1,11 +1,36 @@
 package com.gokimpark.instaclone.profile;
 
+import com.gokimpark.instaclone.member.Member;
+import com.gokimpark.instaclone.member.MemberService;
+import com.gokimpark.instaclone.post.PostService;
+import com.gokimpark.instaclone.story.StoryService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class ProfileService {
+    private final MemberService memberService;
+    private final PostService postService;
+    private final StoryService storyService;
 
-    public Profile getProfile(String userId) {
-        return new Profile();
+    public ProfileService(MemberService memberService, PostService postService, StoryService storyService) {
+        this.memberService = memberService;
+        this.postService = postService;
+        this.storyService = storyService;
+    }
+
+    ModelMapper mapper = new ModelMapper();
+
+    public ProfileDto getProfile(String userId) {
+        Member member = memberService.findByUserId(userId);
+
+        ProfileDto profile = new ProfileDto();
+        profile.setMemberInfo(mapper.map(member, ProfileMemberInfoDto.class));
+        profile.setPosts(mapper.map(postService.findAllByUserId(member.getId()), ProfilePostDto.class));
+        profile.setStories(mapper.map(storyService.findAllByUserId(member.getId()), ProfileStoryDto.class));
+
+        return profile;
     }
 }
+
