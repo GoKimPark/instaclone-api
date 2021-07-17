5a2edbebc47ee2207b203ca296ede06b0c7ed6ee
evelyn82ny <evelyn82ny@gmail.com>
2021-07-18 08:56:20 +0900

    deleted show Story(PostMapping)

--- src/main/java/com/gokimpark/instaclone/story/StoryController.java
+++ src/main/java/com/gokimpark/instaclone/story/StoryController.java
@@ -1,11 +1,8 @@
 package com.gokimpark.instaclone.story;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/stories")
 public class StoryController {
     private final StoryService storyService;
@@ -14,17 +11,8 @@ public class StoryController {
         this.storyService = storyService;
     }
 
-    @ResponseBody
-    @GetMapping("/{UserId}/{StoryId}/")
-    public Story findOnByStoryId (@PathVariable String UserId, @PathVariable Long StoryId){
-        return storyService.findOneByStoryId(StoryId);
-    }
-
-
-    @PostMapping("/{UserId}/{StoryId}/")
-    public String processShowStory(@PathVariable String UserId, @PathVariable Long StoryId, Model model) {
-        Story story = storyService.findOneByStoryId(StoryId);
-        model.addAttribute(story);
-        return "redirect:/stories/{UserId}/{StoryId}/";
+    @GetMapping("/{userId}/{storyId}/")
+    public Story findByStoryId (@PathVariable String userId, @PathVariable Long storyId){
+        return storyService.findOneByStoryId(storyId);
     }
 }
