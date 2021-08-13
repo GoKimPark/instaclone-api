9800cb27d637b31ed3dade095df192f6f496ae39
evelyn82ny <evelyn82ny@gmail.com>
2021-08-13 18:55:47 +0900

    Add like and comment relation to post-entity

--- src/main/java/com/gokimpark/instaclone/domain/post/Post.java
+++ src/main/java/com/gokimpark/instaclone/domain/post/Post.java
@@ -1,29 +1,51 @@
 package com.gokimpark.instaclone.domain.post;
 
+import com.gokimpark.instaclone.domain.comment.Comment;
 import com.gokimpark.instaclone.domain.image.Image;
+import com.gokimpark.instaclone.domain.like.Like;
+import com.gokimpark.instaclone.domain.user.User;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.List;
-import java.util.UUID;
 
 @Entity
-@Getter @Setter
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post {
     @Id
-    @Column(unique = true)
-    private String id;
-
-    private String caption;
-    private String location;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "post")
     private List<Image> images;
 
-    private String username;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "posts")
+    private User user;
 
-    protected Post(){
-        id = UUID.randomUUID().toString();
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "post")
+    private List<Like> likes;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "post")
+    private List<Comment> comments;
+
+    private String caption;
+    private String location;
+
+    @Builder
+    public Post(User user, List<Image> images, String caption, String location){
+        this.user = user;
+        this.images = images;
+        this.caption = caption;
+        this.location = location;
+    }
+
+    public void update(String caption, String location){
+        this.caption = caption;
+        this.location = location;
     }
 }
