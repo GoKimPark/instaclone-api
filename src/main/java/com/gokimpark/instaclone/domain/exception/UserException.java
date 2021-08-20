022b97e7938385048225ed496ddf45d328be012b
evelyn82ny <evelyn82ny@gmail.com>
2021-08-20 12:12:26 +0900

    Add default exception

--- src/main/java/com/gokimpark/instaclone/domain/exception/UserException.java
+++ src/main/java/com/gokimpark/instaclone/domain/exception/UserException.java
@@ -6,7 +6,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class UserException extends RuntimeException{
 
+    public UserException(){
+        super("해당 사용자를 찾을 수 없음.");
+    }
     public UserException(String message){
         super(message);
     }
+
 }
