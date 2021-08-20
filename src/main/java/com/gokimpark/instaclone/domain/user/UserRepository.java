01913b55c0b5f4d95cd2ca53c46727b05f4b7f24
evelyn82ny <evelyn82ny@gmail.com>
2021-08-20 11:58:52 +0900

    Change return type to Optional

--- src/main/java/com/gokimpark/instaclone/domain/user/UserRepository.java
+++ src/main/java/com/gokimpark/instaclone/domain/user/UserRepository.java
@@ -2,10 +2,11 @@ package com.gokimpark.instaclone.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByUsername(String username);
-    User findByEmail(String email);
-
-    User findByEmailOrUsername(String email, String username);
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByEmailOrUsername(String email, String username);
 }
