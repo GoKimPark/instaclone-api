1a89c77353636829e48ba05d1be4ae7208cb91fb
evelyn82ny <evelyn82ny@gmail.com>
2021-09-17 23:10:42 +0900

    Create h2-data

---
+++ src/main/resources/db/h2/data.sql
@@ -0,0 +1,16 @@
+INSERT INTO Users VALUES (1, 'ab123@gmail.com', 'George', 'Franklin', 'pass1', '', '', '', '010123');
+INSERT INTO Users VALUES (2, 'ab456@gmail.com', 'Betty', 'Davis', 'pass2', '', '', '', '0101234');
+INSERT INTO Users VALUES (3, 'ab789@gmail.com', 'Harold', 'Leo', 'pass3', '', '', '', '01012345');
+INSERT INTO Users VALUES (4, 'cd123@gmail.com', 'peter', 'Freddy', 'pass4', '', '', '', '010123456');
+INSERT INTO Users VALUES (5, 'cd456@gmail.com', 'David', 'lucky', 'pass5', '', '', '', '0101234567');
+INSERT INTO Users VALUES (6, 'cd789@gmail.com', 'David', 'sly', 'pass6', '', '', '', '01012345678');
+
+INSERT INTO Post VALUES (1, 'imageUrl', '2021-06-13', 'caption', 'location', 1);
+INSERT INTO Post VALUES (2, 'imageUrl', '2021-06-23', 'caption', 'location', 1);
+INSERT INTO Post VALUES (3, 'imageUrl', '2021-06-23', 'caption', 'location', 2);
+INSERT INTO Post VALUES (4, 'imageUrl', '2021-06-25', 'caption', 'location', 3);
+INSERT INTO Post VALUES (5, 'imageUrl', '2021-07-07', 'caption', 'location', 4);
+INSERT INTO Post VALUES (6, 'imageUrl', '2021-07-17', 'caption', 'location', 4);
+INSERT INTO Post VALUES (7, 'imageUrl', '2021-07-20', 'caption', 'location', 2);
+INSERT INTO Post VALUES (8, 'imageUrl', '2021-08-01', 'caption', 'location', 5);
+INSERT INTO Post VALUES (8, 'imageUrl', '2021-08-02', 'caption', 'location', 6);
