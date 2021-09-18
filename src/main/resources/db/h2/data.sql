12d308183be03cbcf285a9a10940d9912843af15
evelyn82ny <evelyn82ny@gmail.com>
2021-09-18 12:25:59 +0900

    Modify h2

--- src/main/resources/db/h2/data.sql
+++ src/main/resources/db/h2/data.sql
@@ -1,16 +1,13 @@
-INSERT INTO Users VALUES (1, 'ab123@gmail.com', 'George', 'Franklin', 'pass1', '', '', '', '010123');
-INSERT INTO Users VALUES (2, 'ab456@gmail.com', 'Betty', 'Davis', 'pass2', '', '', '', '0101234');
-INSERT INTO Users VALUES (3, 'ab789@gmail.com', 'Harold', 'Leo', 'pass3', '', '', '', '01012345');
-INSERT INTO Users VALUES (4, 'cd123@gmail.com', 'peter', 'Freddy', 'pass4', '', '', '', '010123456');
-INSERT INTO Users VALUES (5, 'cd456@gmail.com', 'David', 'lucky', 'pass5', '', '', '', '0101234567');
-INSERT INTO Users VALUES (6, 'cd789@gmail.com', 'David', 'sly', 'pass6', '', '', '', '01012345678');
+INSERT INTO Users (email, name, username, password, profile_image_url, website, bio, phone_number) VALUES ('ab123@gmail.com', 'George', 'Franklin', 'pass100', 'url', 'web', 'bio', '77123');
+INSERT INTO Users (email, name, username, password, profile_image_url, website, bio, phone_number) VALUES ('ab456@gmail.com', 'Betty', 'Davis', 'pass200', 'url', 'web', 'bio', '31234');
+INSERT INTO Users (email, name, username, password, profile_image_url, website, bio, phone_number) VALUES ('ab789@gmail.com', 'Harold', 'Leo', 'pass300', 'url', 'web', 'bio', '0982345');
+INSERT INTO Users (email, name, username, password, profile_image_url, website, bio, phone_number) VALUES ('cd123@gmail.com', 'peter', 'Freddy', 'pass400', 'url', 'web', 'bio', '012154');
+INSERT INTO Users (email, name, username, password, profile_image_url, website, bio, phone_number) VALUES ('cd456@gmail.com', 'David', 'lucky', 'pass500', 'url', 'web', 'bio', '54453');
+INSERT INTO Users (email, name, username, password, profile_image_url, website, bio, phone_number) VALUES ('cd789@gmail.com', 'David', 'sly', 'pass600', 'url', 'web', 'bio', '47');
 
-INSERT INTO Post VALUES (1, 'imageUrl', '2021-06-13', 'caption', 'location', 1);
-INSERT INTO Post VALUES (2, 'imageUrl', '2021-06-23', 'caption', 'location', 1);
-INSERT INTO Post VALUES (3, 'imageUrl', '2021-06-23', 'caption', 'location', 2);
-INSERT INTO Post VALUES (4, 'imageUrl', '2021-06-25', 'caption', 'location', 3);
-INSERT INTO Post VALUES (5, 'imageUrl', '2021-07-07', 'caption', 'location', 4);
-INSERT INTO Post VALUES (6, 'imageUrl', '2021-07-17', 'caption', 'location', 4);
-INSERT INTO Post VALUES (7, 'imageUrl', '2021-07-20', 'caption', 'location', 2);
-INSERT INTO Post VALUES (8, 'imageUrl', '2021-08-01', 'caption', 'location', 5);
-INSERT INTO Post VALUES (8, 'imageUrl', '2021-08-02', 'caption', 'location', 6);
+INSERT INTO Post (image_url, created_time, caption, location, user_id) VALUES ('imageUrl', '2021-06-13', 'caption', 'location', 1);
+INSERT INTO Post (image_url, created_time, caption, location, user_id) VALUES ('imageUrl', '2021-06-23', 'caption', 'location', 1);
+INSERT INTO Post (image_url, created_time, caption, location, user_id) VALUES ('imageUrl', '2021-06-23', 'caption', 'location', 2);
+INSERT INTO Post (image_url, created_time, caption, location, user_id) VALUES ('imageUrl', '2021-06-25', 'caption', 'location', 3);
+INSERT INTO Post (image_url, created_time, caption, location, user_id) VALUES ('imageUrl', '2021-07-07', 'caption', 'location', 4);
+INSERT INTO Post (image_url, created_time, caption, location, user_id) VALUES ('imageUrl', '2021-07-17', 'caption', 'location', 4);
--- src/main/resources/db/h2/schema.sql
+++ src/main/resources/db/h2/schema.sql
@@ -1,50 +1,51 @@
-DROP TABLE Users IF EXISTS;
-DROP TABLE Post IF EXISTS;
-DROP TABLE Comment IF EXISTS;
-DROP TABLE Likes IF EXISTS;
-DROP TABLE Follow IF EXISTS;
+DROP TABLE IF EXISTS Comment;
+DROP TABLE IF EXISTS Likes;
+DROP TABLE IF EXISTS Post;
+DROP TABLE IF EXISTS Users;
 
 CREATE TABLE Users (
-    id              INTEGER IDENTITY PRIMARY KEY,
+    id              INTEGER AUTO_INCREMENT PRIMARY KEY,
     email           VARCHAR(30),
     name            VARCHAR(30),
     username        VARCHAR(30),
     password        VARCHAR(30),
-    profileImageUrl VARCHAR(30),
-    webSite         VARCHAR(30),
+    profile_image_url VARCHAR(30),
+    website         VARCHAR(30),
     bio             VARCHAR(30),
-    phoneNumber     VARCHAR(30)
+    phone_number     VARCHAR(30)
 );
 
 CREATE TABLE Post (
-    id         INTEGER IDENTITY PRIMARY KEY,
+    id         INTEGER IDENTITY AUTO_INCREMENT PRIMARY KEY,
     imageURL   varchar(30),
-    createDate DATE,
+    createdTime CURRENT_DATE,
     caption    varchar(30),
     location   varchar(30),
-    user       INTEGER NOT NULL
+    user_id    INTEGER NOT NULL
 );
 ALTER TABLE Post ADD CONSTRAINT fk_post_user FOREIGN KEY (user_id) REFERENCES Users (id);
 
 CREATE TABLE Comment (
-    id   INTEGER IDENTITY PRIMARY KEY,
-    text VARCHAR(30),
-    post INTEGER NOT NULL,
-    user INTEGER NOT NULL
+    id      INTEGER IDENTITY PRIMARY KEY,
+    text    VARCHAR(30),
+    post_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    createdTime CURRENT_DATE
 );
 ALTER TABLE Comment ADD CONSTRAINT fk_comment_post FOREIGN KEY (post_id) REFERENCES Post (id);
 ALTER TABLE Comment ADD CONSTRAINT fk_comment_user FOREIGN KEY (user_id) REFERENCES Users (id);
 
 CREATE TABLE Likes (
-    id   INTEGER IDENTITY PRIMARY KEY,
-    post INTEGER NOT NULL,
-    user INTEGER NOT NULL
+    id      INTEGER IDENTITY PRIMARY KEY,
+    post_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL
 );
 ALTER TABLE Likes ADD CONSTRAINT fk_likes_post FOREIGN KEY (post_id) REFERENCES Post (id);
 ALTER TABLE Likes ADD CONSTRAINT fk_likes_user FOREIGN KEY (user_id) REFERENCES Users (id);
 
+/*
 CREATE TABLE FOLLOW (
     toUser INTEGER NOT NULL,
     fromUser INTEGER NOT NULL,
     constraint pk_follow PRIMARY KEY (toUser, fromUser)
-);
+);*/
