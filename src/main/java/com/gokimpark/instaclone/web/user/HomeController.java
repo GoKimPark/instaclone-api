88a90fdae93aed17e9b53c83d63c2be6b0bb8a6f
nayoung <evelyn82ny@gmail.com>
2021-09-25 22:16:24 +0900

    Create home-controller

---
+++ src/main/java/com/gokimpark/instaclone/web/user/HomeController.java
@@ -0,0 +1,17 @@
+package com.gokimpark.instaclone.web.user;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class HomeController {
+
+    @GetMapping("/home")
+    public void home() {
+        System.out.println("hello world\n");
+    }
+}
