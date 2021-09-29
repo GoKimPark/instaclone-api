868a1a12b7d6be0dcaaee5bac5fd354260b0b76e
nayoung <evelyn82ny@gmail.com>
2021-09-30 00:06:01 +0900

    Create UserDto

---
+++ src/main/java/com/gokimpark/instaclone/domain/user/dto/UserDto.java
@@ -0,0 +1,33 @@
+package com.gokimpark.instaclone.domain.user.dto;
+
+import lombok.Data;
+import org.springframework.data.annotation.ReadOnlyProperty;
+
+import javax.persistence.Column;
+import javax.persistence.Lob;
+import javax.validation.constraints.Email;
+
+@Data
+public class UserDto {
+
+    @ReadOnlyProperty
+    private Integer id;
+
+    @Email @Column(nullable = false)
+    private String email;
+    private String name;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+    private String profileImageUrl;
+    private String webSite;
+
+    @Lob
+    private String bio;
+
+    @Column(unique = true)
+    private String phoneNumber;
+}
