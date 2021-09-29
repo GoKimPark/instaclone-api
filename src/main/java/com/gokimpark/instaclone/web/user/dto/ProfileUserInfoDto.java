66e16961ba5f4633304c9bf41b5b395ece39af63
nayoung <evelyn82ny@gmail.com>
2021-09-30 00:15:51 +0900

    Change the count's data type from String to Long

--- src/main/java/com/gokimpark/instaclone/web/user/dto/ProfileUserInfoDto.java
+++ src/main/java/com/gokimpark/instaclone/web/user/dto/ProfileUserInfoDto.java
@@ -1,6 +1,5 @@
 package com.gokimpark.instaclone.web.user.dto;
 
-import com.gokimpark.instaclone.domain.user.User;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
@@ -20,24 +19,9 @@ public class ProfileUserInfoDto {
     private String bio;
 
     @NotBlank
-    private String postCount;
+    private Long postCount;
     @NotBlank
-    private String followerCount;
+    private Long followerCount;
     @NotBlank
-    private String followingCount;
-
-
-    public static ProfileUserInfoDto from(User user, String postCount, String followerCount, String followingCount) {
-        ProfileUserInfoDto dto = new ProfileUserInfoDto();
-        dto.setProfileImageUrl(user.getProfileImageUrl());
-        dto.setUsername(user.getUsername());
-        dto.setName(user.getName());
-        dto.setBio(user.getBio());
-        dto.setWebSite(user.getWebsite());
-
-        dto.postCount = postCount;
-        dto.followerCount = followerCount;
-        dto.followingCount = followingCount;
-        return dto;
-    }
+    private Long followingCount;
 }
