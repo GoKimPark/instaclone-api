fdeb52a460980c11a964a820801794ca4ddcc74d
nayoung <evelyn82ny@gmail.com>
2022-03-11 01:08:56 +0900

    feat: add name field of user entity

--- src/main/java/com/gokimpark/instaclone/domain/follow/dto/FollowSimpleListDto.java
+++ src/main/java/com/gokimpark/instaclone/domain/follow/dto/FollowSimpleListDto.java
@@ -7,9 +7,11 @@ public class FollowSimpleListDto {
 
     private String profileImageUrl;
     private String username;
+    private String name;
 
-    public FollowSimpleListDto(String username) {
+    public FollowSimpleListDto(String username, String name) {
         this.profileImageUrl = null;
         this.username = username;
+        this.name = name;
     }
 }
