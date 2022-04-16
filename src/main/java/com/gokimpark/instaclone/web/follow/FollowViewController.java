230cb9a3915f7e8de871e8ca707441fedb5c9008
nayoung <evelyn82ny@gmail.com>
2022-04-17 02:05:39 +0900

    refactor: Change the criteria for following and following lists
    - 사용자A가 사용자B의 팔로잉 목록을 요청할 때 해당되는 사용자들과의 팔로잉 상태가 사용자A를 기준으로 판단되어야함

---
+++ src/main/java/com/gokimpark/instaclone/web/follow/FollowViewController.java
@@ -0,0 +1,49 @@
+package com.gokimpark.instaclone.web.follow;
+
+import com.gokimpark.instaclone.domain.follow.FollowService;
+import com.gokimpark.instaclone.domain.follow.dto.UserSimpleInfoDto;
+import com.gokimpark.instaclone.domain.user.ProfileService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class FollowViewController {
+
+    private final FollowService followService;
+    private final ProfileService profileService;
+
+    @PostMapping("/follow/{toUsername}/{fromUsername}")
+    public String addFollow(@PathVariable String toUsername, @PathVariable String fromUsername){
+        Boolean result = followService.addFollow(toUsername, fromUsername);
+        return "redirect:/profile/{toUsername}/{fromUsername}";
+    }
+
+    @PostMapping("/unfollow/{toUsername}/{fromUsername}")
+    public String unFollow(@PathVariable String toUsername, @PathVariable String fromUsername){
+        Boolean result = followService.unFollow(toUsername, fromUsername);
+        return "redirect:/profile/{toUsername}/{fromUsername}";
+    }
+
+    @GetMapping("/follower/{fromUsername}/{requestedUsername}")
+    public String getFollower(@PathVariable String fromUsername, @PathVariable String requestedUsername, Model model){
+        List<UserSimpleInfoDto> followerList = followService.getFollowRelationList("follower", fromUsername, requestedUsername);
+        model.addAttribute("users", followerList);
+        return "/account/userList";
+    }
+
+    @GetMapping("/following/{fromUsername}/{requestedUsername}")
+    public String getFollowing(@PathVariable String fromUsername, @PathVariable String requestedUsername, Model model){
+        List<UserSimpleInfoDto> followingList = followService.getFollowRelationList("following", fromUsername, requestedUsername);
+        model.addAttribute("users", followingList);
+        return "/account/userList";
+    }
+}
